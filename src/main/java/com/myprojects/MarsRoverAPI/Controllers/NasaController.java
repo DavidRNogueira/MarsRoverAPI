fea2bca2c60edd829a377fa7fadd6f572a6947c2
davidrnogueira <davidrnogueira@hotmail.com>
2020-05-20 17:34:46 -0700

    BackEnd work

--- src/main/java/com/myprojects/MarsRoverAPI/Controllers/NasaController.java
+++ src/main/java/com/myprojects/MarsRoverAPI/Controllers/NasaController.java
@@ -1,14 +1,21 @@
 package com.myprojects.MarsRoverAPI.Controllers;
 
+import com.myprojects.MarsRoverAPI.Dto.PhotosListDto;
+import com.myprojects.MarsRoverAPI.Service.NasaService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NasaController {
+  private NasaService nasaService;
+
+  public NasaController (NasaService nasaService) {
+    this.nasaService = nasaService;
+  }
 
   @GetMapping("/get-images-by-date")
-  public void getImagesByDate (@RequestParam(name="date") String date) {
-
+  public PhotosListDto getImagesByDate (@RequestParam(name="date") String date) {
+    return nasaService.makeRequestForImages(date);
   }
 }
